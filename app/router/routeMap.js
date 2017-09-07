6d9debe7169942c86bc949d44b8f9c1791efa6d4
was <zxt_zel_npl@163.com>
2017-09-07 20:53:54 +0800

    home-2017-09-07

--- app/router/routeMap.js
+++ app/router/routeMap.js
@@ -21,14 +21,14 @@ class RouterMap extends React.Component{
   render(){
     return(
       <Router history={this.props.history}>
-          <Route path='/' component={App}>
-            <IndexRoute component={Home} />
-            <Route path='/city' component={City}/>
-            <Route path='/user' component={User}/>
-            <Route path='/search/:type(/:keyword)' component={Search}/>
-            <Route path='/detail/:id' component={Detail}/>
-            <Route path='*' component={NotFound}/>
-          </Route>
+        <Route path='/' component={App}>
+          <IndexRoute component={Home} />
+          <Route path='/city' component={City}/>
+          <Route path='/user' component={User}/>
+          <Route path='/search/:type(/:keyword)' component={Search}/>
+          <Route path='/detail/:id' component={Detail}/>
+          <Route path='*' component={NotFound}/>
+        </Route>
       </Router>
     )
   }
